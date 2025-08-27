5a499bd2ea3ddd20abba6b3779e2df844d014fc2
Christian Enevoldsen <c.enevoldsen@gmail.com>
2025-08-27 22:32:35 +0200

    feat: add api-auth library with controller, service, and module setup

---
+++ libs/api/auth/src/lib/api-auth.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { ApiAuthService } from './api-auth.service';
+
+@Controller('api-auth')
+export class ApiAuthController {
+  constructor(private apiAuthService: ApiAuthService) {}
+}
