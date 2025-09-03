ba1c275adc2e0bb12e878be2d4ca5bf5de45bb2a
Christian Enevoldsen <c.enevoldsen@gmail.com>
2025-09-03 21:45:21 +0200

    feat: provide PrimeNG and animations support

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,16 +1,25 @@
+import { provideHttpClient } from '@angular/common/http';
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
 import { appRoutes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    }),
     provideHttpClient(),
     provideRouter(appRoutes),
   ],
